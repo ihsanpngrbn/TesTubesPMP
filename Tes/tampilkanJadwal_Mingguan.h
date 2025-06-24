e24655e400363d6d7fccbdb08f5821894740b0f3
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 16:09:26 +0700

    Create tampilkanJadwal_Mingguan.h

---
+++ Tes/tampilkanJadwal_Mingguan.h
@@ -0,0 +1,12 @@
+#ifndef TAMPILKAN_JADWAL_MINGGUAN_H
+#define TAMPILKAN_JADWAL_MINGGUAN_H
+
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <string.h>   
+
+#inlcude "program_buat_jadwal.h"
+
+void tampilkanJadwal_Mingguan();
+
+#endif // TAMPILKAN_JADWAL_MINGGUAN_H
