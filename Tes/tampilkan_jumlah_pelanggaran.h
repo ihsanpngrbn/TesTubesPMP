76f7e1c3cef40089c0fa455ceed4f665d6d960fa
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 18:48:47 +0700

    Create tampilkan_jumlah_pelanggaran.h

---
+++ Tes/tampilkan_jumlah_pelanggaran.h
@@ -0,0 +1,9 @@
+#ifndef TAMPILKAN_JUMLAH_PELANGGARAN_H
+#define TAMPILKAN_JUMLAH_PELANGGARAN_H
+
+#include <stdio.h> 
+#include "program_buat_jadwal.h" 
+
+void tampilkanJumlahPelanggaran();
+
+#endif // TAMPILKAN_JUMLAH_PELANGGARAN_H
