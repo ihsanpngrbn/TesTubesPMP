276c67376bf04228f6fd6142582ac2437c04e13f
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 18:52:25 +0700

    Update menu_tampilkan_jadwal.c

--- Tes/menu_tampilkan_jadwal.c
+++ Tes/menu_tampilkan_jadwal.c
@@ -5,6 +5,9 @@
 
 #include "tampilkanJadwal_Harian.h"
 #include "tampilkanJadwal_Mingguan.h"
+#include "tampilkanJadwal_Bulanan.h"
+#include "tampilkan_jumlah_pelanggaran.h"
+#include "tampilkan_jumlah_shift.h"
 
 
 void menu_tampilkan_jadwal() {
@@ -14,7 +17,10 @@ void menu_tampilkan_jadwal() {
         printf("\nTampilkan Jadwal\n");
         printf("1. Tampilkan Jadwal Harian\n");
         printf("2. Tampilkan Jadwal Mingguan\n");
-        printf("3. Kembali ke Menu Utama\n");
+        printf("3. Tampilkan Jadwal Bulanan\n");
+        printf("4. Tampilkan Jumlah Pelanggaran\n");
+        printf("5. Tampilkan Jumlah Shift Masing-Masing Dokter\n");
+        printf("6. Kembali ke Menu Utama\n");
         printf("Masukkan pilihan (1-3): ");
         scanf("%d", &pilihan);
         
@@ -26,13 +32,25 @@ void menu_tampilkan_jadwal() {
             case 2:
                 tampilkanJadwal_Mingguan();
                 break;
-
+            
             case 3:
+                tampilkanJadwal_Bulanan();
+                break;
+
+            case 4:
+                tampilkan_jumlah_pelanggaran();
+                break;
+
+            case 5:
+                tampilkan_jumlah_shift();
+                break;
+
+            case 6:
                 printf("Kembali ke menu utama\n");
                 return;
                 
             default:
-                printf("Pilihan tidak valid. Masukkan angka 1-3.\n");
+                printf("Pilihan tidak valid. Masukkan angka 1-6.\n");
         }
     }
 }
