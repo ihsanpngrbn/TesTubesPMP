feeefa668840956a5ca2bf784fa1b16902acab88
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 15:52:57 +0700

    Create menu_tampilkan_jadwal.c

---
+++ Tes/menu_tampilkan_jadwal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "MenampilkanJadwal_Harian.h"
+#include "MenampilkanJadwal_Mingguan.h"
+#include "MenampilkanJadwal_Bulanan.h"
+
+
+void menu_tampilkan_jadwal() {
+    int pilihan;
+    
+    while (1) {
+        printf("\nTampilkan Jadwal\n");
+        printf("1. Tampilkan Jadwal Harian\n");
+        printf("2. Tampilkan Jadwal Mingguan\n");
+        printf("3. Kembali ke Menu Utama\n");
+        printf("Masukkan pilihan (1-3): ");
+        scanf("%d", &pilihan);
+        
+        switch (pilihan) {
+            case 1:
+                tampilkanJadwal_Harian();
+                break;
+
+            case 2:
+                tampilkanJadwal_Mingguan();
+                break;
+
+            case 3:
+                printf("Kembali ke menu utama\n");
+                return;
+                
+            default:
+                printf("Pilihan tidak valid. Masukkan angka 1-3.\n");
+        }
+    }
+}
