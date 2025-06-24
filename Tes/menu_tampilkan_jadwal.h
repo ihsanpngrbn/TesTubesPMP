1a668254da4d8dbdb18fb12441b7d10d1be19cbc
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 15:53:26 +0700

    Create menu_tampilkan_jadwal.h

---
+++ Tes/menu_tampilkan_jadwal.h
@@ -0,0 +1,16 @@
+#ifndef MENU_TAMPILKAN_JADWAL_H
+#define MENU_TAMPILKAN_JADWAL_H
+
+#include <stdio.h>    // Untuk printf
+#include <stdlib.h>   // Untuk fungsi utilitas umum
+#include <string.h>   // Untuk manipulasi string
+
+#include "MenampilkanJadwal_Harian.h"
+#include "MenampilkanJadwal_Mingguan.h"
+#include "MenampilkanJadwal_Bulanan.h" // Meskipun belum dipanggil, Anda meng-include ini di .c
+
+
+// Deklarasi fungsi menu_tampilkan_jadwal
+void menu_tampilkan_jadwal();
+
+#endif // MENU_TAMPILKAN_JADWAL_H
