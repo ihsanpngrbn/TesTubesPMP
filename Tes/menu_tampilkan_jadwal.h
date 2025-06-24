04ab6c15f70c9340b2c097ebe76879ec1e031e5a
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 18:53:22 +0700

    Update menu_tampilkan_jadwal.h

--- Tes/menu_tampilkan_jadwal.h
+++ Tes/menu_tampilkan_jadwal.h
@@ -1,14 +1,16 @@
 #ifndef MENU_TAMPILKAN_JADWAL_H
 #define MENU_TAMPILKAN_JADWAL_H
 
-#include <stdio.h>    // Untuk printf
-#include <stdlib.h>   // Untuk fungsi utilitas umum
-#include <string.h>   // Untuk manipulasi string
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <string.h>   
 
 #include "tampilkanJadwal_Harian.h"
 #include "tampilkanJadwal_Mingguan.h"
+#include "tampilkanJadwal_Bulanan.h"
+#include "tampilkan_jumlah_pelanggaran.h"
+#include "tampilkan_jumlah_shift.h"
 
-// Deklarasi fungsi menu_tampilkan_jadwal
 void menu_tampilkan_jadwal();
 
 #endif // MENU_TAMPILKAN_JADWAL_H
