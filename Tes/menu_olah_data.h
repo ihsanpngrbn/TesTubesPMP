9be5733dede90db3b7ffca1ce38d9d64514e6da8
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 15:04:59 +0700

    Create menu_olah_data.h

---
+++ Tes/menu_olah_data.h
@@ -0,0 +1,20 @@
+#ifndef OLAH_DATA_H
+#define OLAH_DATA_H
+
+#include <stdio.h>   
+#include <stdlib.h>  
+#include <string.h>  
+
+// Fungsi untuk menambahkan data dokter baru
+void tambahDokter();
+
+// Fungsi untuk menghapus data dokter
+void hapusDokter();
+
+// Fungsi untuk menampilkan semua data dokter
+void tampilkanData();
+
+
+void menu_olah_data();
+
+#endif // OLAH_DATA_H
