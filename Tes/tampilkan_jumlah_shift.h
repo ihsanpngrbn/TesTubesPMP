ff2c0932196122218c505383130f51193dfa6fff
ihsanpngrbn <151978570+ihsanpngrbn@users.noreply.github.com>
2025-06-24 19:00:12 +0700

    Create tampilkan_jumlah_shift.h

---
+++ Tes/tampilkan_jumlah_shift.h
@@ -0,0 +1,10 @@
+#ifndef TAMPILKAN_JUMLAH_SHIFT_H
+#define TAMPILKAN_JUMLAH_SHIFT_H
+
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <string.h>   
+
+void tampilkan_jumlah_shift();
+
+#endif // TAMPILKAN_JUMLAH_SHIFT_H
